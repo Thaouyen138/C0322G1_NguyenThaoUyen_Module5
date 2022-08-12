6720c80bcd4807a31d021e3c751b15bb6f4e00cb
Thaouyen138 <96711178+Thaouyen138@users.noreply.github.com>
2022-08-12 21:38:24 +0700

    update

---
+++ module5/_02_typescript/exercise/fibonaci/src/fibonacci.js
@@ -0,0 +1,22 @@
+function totalFibonacci() {
+    var fibonacciArr = [0, 1];
+    var total = 1;
+    var a = 0;
+    var b = 1;
+    var c = 0;
+    while (true) {
+        if (a + b == c) {
+            fibonacciArr.push(c);
+            a = b;
+            b = c;
+            total += c;
+        }
+        c++;
+        if (fibonacciArr.length == 20) {
+            break;
+        }
+    }
+    console.log(fibonacciArr.toString());
+    console.log(total);
+}
+totalFibonacci();
