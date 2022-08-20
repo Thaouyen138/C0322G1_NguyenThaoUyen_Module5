7b0e384e031b00f7178b899608c4dcb3118df682
Thaouyen138 <123uyen234@gmail.com>
2022-08-21 02:21:08 +0700

    case study

--- module5/_casestudy_module5/FuramaAngular/furama/src/app/customer/customer.component.ts
+++ module5/_casestudy_module5/FuramaAngular/furama/src/app/customer/customer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Customer} from "./customer";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-customer',
@@ -53,9 +54,39 @@ customerList: Customer[] =[
     address: 'USA'
   }
 ]
+  customerForm = new FormGroup({
+    id: new FormControl('', Validators.required),
+    customerType: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
+    dateOfBirth: new FormControl('', Validators.required),
+    gender: new FormControl('', Validators.required),
+    indentity: new FormControl('', [Validators.required,Validators.pattern('^[0-9]{9}')]),
+    phone: new FormControl('' , [Validators.required, Validators.pattern('^\\+84\\d{9,10}$')]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    address: new FormControl('', Validators.required)
+  })
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    const  customer = this.customerForm.value;
+    this.customerList.push(customer);
+    this.customerForm.reset()
+  }
+
+  edit(customer: Customer) {
+    this.customerForm.patchValue(customer)
+  }
+
+  update() {
+    const customer = this.customerForm.value;
+    console.log(customer)
+    for (let i = 0; i < this.customerList.length; i++) {
+      if (this.customerList[i].id === customer.id) {
+        this.customerList[i] = customer;
+      }
+    }
+  }
 }
